b6cf2d9c3b000726654da4e424f5934db015cb06
CSPon <hsmchul@hotmail.com>
2017-09-20 11:09:52 -0500

    Auto-generated comment

--- push_all.sh
+++ push_all.sh
@@ -1,7 +1,8 @@
-@echo "Adding all files"
+clear
+echo "Adding all files"
 git add *
-@echo "Committing all files"
+echo "Committing all files"
 git commit -m "Auto-generated comment"
-@echo "Pushing all files"
+echo "Pushing all files"
 git push origin master
 
